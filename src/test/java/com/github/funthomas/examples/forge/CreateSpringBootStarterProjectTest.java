e9c14d512cf26bcd61fdd46aee0e2c2f9dc0c764
Thomas Schubert <funthomas424242@gmail.com>
2015-05-18 23:12:40 +0200

    test case created but not working
    Signed-off-by: Thomas Schubert <funthomas424242@gmail.com>

---
+++ src/test/java/com/github/funthomas/examples/forge/CreateSpringBootStarterProjectTest.java
@@ -0,0 +1,17 @@
+package com.github.funthomas.examples.forge;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CreateSpringBootStarterProjectTest {
+
+	
+	
+	@Test
+	@Ignore
+	public void test() {
+		final CreateSpringBootStarterProject excecutor = new CreateSpringBootStarterProject();
+		excecutor.run();
+	}
+
+}
